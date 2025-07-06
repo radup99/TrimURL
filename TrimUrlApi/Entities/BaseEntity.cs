f2caef2414c67cd547d75fd9d06cec2e22d8a11b
radup99 <raduplacinta85@gmail.com>
2025-07-06 17:57:12 +0300

    Create BaseEntity and ShortUrl classes

---
+++ TrimUrlApi/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrimUrlApi.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ TrimUrlApi/Entities/ShortUrl.cs
@@ -0,0 +1,13 @@
+﻿namespace TrimUrlApi.Entities
+{
+    public class ShortUrl : BaseEntity
+    {
+        public int CreatorId { get; set; }
+
+        public string Url { get; set; }
+
+        public string Code { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}
