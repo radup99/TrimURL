fcddd7ac3feafc4179f57eae06676182094dd7ea
radup99 <raduplacinta85@gmail.com>
2025-07-07 01:25:11 +0300

    Create MainDbContext and initial migration

---
+++ TrimUrlApi/Database/MainDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrimUrlApi.Entities;
+
+namespace TrimUrlApi.Database
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+
+        public MainDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Database.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortUrl>().ToTable("ShortUrls");
+        }
+    }
+}
--- TrimUrlApi/Program.cs
+++ TrimUrlApi/Program.cs
@@ -1,3 +1,4 @@
+using TrimUrlApi.Database;
 
 namespace TrimUrlApi
 {
@@ -13,6 +14,7 @@ namespace TrimUrlApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<MainDbContext>();
 
             var app = builder.Build();
 
