fb33568332a48b3c48cb32cd9614c33c697320fe
radup99 <raduplacinta85@gmail.com>
2025-07-08 18:17:19 +0300

    Create GET and POST models for ShortUrl

---
+++ TrimUrlApi/Models/ShortUrlGetModel.cs
@@ -0,0 +1,17 @@
+﻿using TrimUrlApi.Entities;
+
+namespace TrimUrlApi.Models
+{
+    public class ShortUrlGetModel
+    {
+        public string Url { get; set; }
+
+        public string Code { get; set; }
+
+        public ShortUrlGetModel(ShortUrl shortUrl)
+        {
+            Url = shortUrl.Url;
+            Code = shortUrl.Code;
+        }
+    }
+}
---
+++ TrimUrlApi/Models/ShortUrlPostModel.cs
@@ -0,0 +1,11 @@
+﻿using TrimUrlApi.Entities;
+
+namespace TrimUrlApi.Models
+{
+    public class ShortUrlPostModel
+    {
+        public string Url { get; set; }
+
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
