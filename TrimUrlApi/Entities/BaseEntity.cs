2a47340650298f8e8862b8d74449414b12cf151b
radup99 <raduplacinta85@gmail.com>
2025-07-12 15:56:48 +0300

    Update BaseEntity and ShortUrlGetModel

--- TrimUrlApi/Entities/BaseEntity.cs
+++ TrimUrlApi/Entities/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace TrimUrlApi.Entities
 {
@@ -7,8 +8,13 @@ namespace TrimUrlApi.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonPropertyOrder(-3)]
         public int Id { get; set; }
+
+        [JsonPropertyOrder(-2)]
         public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyOrder(-1)]
         public DateTime UpdatedAt { get; set; }
     }
 }
--- TrimUrlApi/Models/ShortUrlGetModel.cs
+++ TrimUrlApi/Models/ShortUrlGetModel.cs
@@ -4,14 +4,20 @@ namespace TrimUrlApi.Models
 {
     public class ShortUrlGetModel
     {
+        public int Id { get; set; }
+
         public string Url { get; set; }
 
         public string Code { get; set; }
 
+        public int AccessCount { get; set; }
+
         public ShortUrlGetModel(ShortUrl shortUrl)
         {
+            Id = shortUrl.Id;
             Url = shortUrl.Url;
             Code = shortUrl.Code;
+            AccessCount = shortUrl.AccessCount;
         }
     }
 }
