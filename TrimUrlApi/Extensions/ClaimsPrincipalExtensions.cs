409d4befda186d649aad3c87f77b576f9c221675
radup99 <raduplacinta85@gmail.com>
2025-08-09 23:08:24 +0300

    Create ClaimsPrincipalExtensions class

---
+++ TrimUrlApi/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using TrimUrlApi.Entities;
+
+namespace TrimUrlApi.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int? GetAuthUserId(this ClaimsPrincipal user)
+        {
+            var sub = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return sub != null ? Int32.Parse(sub) : null;
+        }
+
+        public static string? GetAuthUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst("username")?.Value;
+        }
+    }
+}
