ca99f934fde1f8ad590ce5c623d1cae22545928a
radup99 <raduplacinta85@gmail.com>
2026-07-18 02:01:43 +0300

    Set length constraints on User entity and models

--- TrimUrlApi/Models/UserPostModel.cs
+++ TrimUrlApi/Models/UserPostModel.cs
@@ -6,16 +6,20 @@ namespace TrimUrlApi.Models
     public class UserPostModel
     {
         [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; }
 
         [Required]
+        [StringLength(30, MinimumLength = 10)]
         public string Password { get; set; }
 
         [Required]
         [EmailAddress]
+        [StringLength(254)]
         public string EmailAddress { get; set; }
 
         [Required]
+        [StringLength(100, MinimumLength = 5)]
         public string FullName { get; set; }
     }
 }
