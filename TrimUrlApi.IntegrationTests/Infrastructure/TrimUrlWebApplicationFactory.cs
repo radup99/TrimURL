08b514d9a24ca686d2de159bd2559aeaed860b5a
raduplacinta99 <raduplacinta85@gmail.com>
2026-08-03 23:13:50 +0300

    Setup Integration Testing project and WebApplicationFactory

---
+++ TrimUrlApi.IntegrationTests/Infrastructure/TrimUrlWebApplicationFactory.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TrimUrlApi.Database;
+
+namespace TrimUrlApi.IntegrationTests.Infrastructure;
+
+public class TrimUrlWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly SqliteConnection _connection;
+
+    public TrimUrlWebApplicationFactory()
+    {
+        _connection = new SqliteConnection("DataSource=:memory:");
+        _connection.Open();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Jwt:Secret"] = "ThisIsAReallyLongSecretKeyForTests123!",
+                ["JwtSettings:Issuer"] = "TrimUrlTest",
+                ["JwtSettings:Audience"] = "TrimUrlTest"
+            });
+        });
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<MainDbContext>>();
+            services.RemoveAll<IDbContextOptionsConfiguration<MainDbContext>>();
+
+            services.AddDbContext<MainDbContext>(options =>
+            {
+                options.UseSqlite(_connection);
+            });
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        if (disposing)
+            _connection.Dispose();
+    }
+}
