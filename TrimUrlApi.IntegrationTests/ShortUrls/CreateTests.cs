ab86c323b825906de7f0f86a50777e56ad20ff52
raduplacinta99 <raduplacinta85@gmail.com>
2026-08-07 01:14:58 +0300

    Add tests for ShortUrl Create/Get

---
+++ TrimUrlApi.IntegrationTests/ShortUrls/CreateTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http.Json;
+using TrimUrlApi.Database;
+using TrimUrlApi.IntegrationTests.Infrastructure;
+
+namespace TrimUrlApi.IntegrationTests.ShortUrls
+{
+    public class CreateTests : IClassFixture<TrimUrlWebApplicationFactory>
+    {
+        private readonly TrimUrlWebApplicationFactory _factory;
+        private readonly HttpClient _client;
+
+        public CreateTests(TrimUrlWebApplicationFactory factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+
+            using var scope = factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            db.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnCreated()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.PostAsJsonAsync(
+                ApiRoutes.ShortUrls,
+                new
+                {
+                    Url = "https://www.google.com/"
+                });
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnBadRequest_WhenUrlIsInvalid()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.PostAsJsonAsync(
+                ApiRoutes.ShortUrls,
+                new
+                {
+                    Url = "not-a-url"
+                });
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnBadRequest_WhenRequestBodyIsInvalid()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.PostAsJsonAsync(
+                ApiRoutes.ShortUrls,
+                new
+                {
+                    Website = "https://www.google.com/"
+                });
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ TrimUrlApi.IntegrationTests/ShortUrls/GetTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http.Json;
+using TrimUrlApi.Database;
+using TrimUrlApi.IntegrationTests.Infrastructure;
+
+namespace TrimUrlApi.IntegrationTests.ShortUrls
+{
+    public class GetTests : IClassFixture<TrimUrlWebApplicationFactory>
+    {
+        private readonly TrimUrlWebApplicationFactory _factory;
+        private readonly HttpClient _client;
+
+        public GetTests(TrimUrlWebApplicationFactory factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+
+            using var scope = factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            db.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public async Task GetByCode_ShouldReturnShortUrl()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            using var scope = _factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+
+            const string validCode = "abc123";
+
+            await DatabaseSeeder.SeedShortUrlAsync(
+                db,
+                code: validCode,
+                url: "https://google.com"
+            );
+
+
+            var response = await _client.GetAsync(
+                ApiRoutes.ShortUrlByCode(validCode)
+             );
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        [Fact]
+        public async Task GetByCode_ShouldReturnNotFound_WhenCodeDoesNotExist()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            using var scope = _factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+
+            const string invalidCode = "def456";
+
+            await DatabaseSeeder.SeedShortUrlAsync(
+                db,
+                code: "abc123",
+                url: "https://google.com"
+            );
+
+
+            var response = await _client.GetAsync(
+                ApiRoutes.ShortUrlByCode(invalidCode)
+             );
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByCode_ShouldReturnGone_WhenShortUrlIsExpired()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            using var scope = _factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+
+            const string expiredCode = "abc123";
+
+            await DatabaseSeeder.SeedShortUrlAsync(
+                db,
+                code: expiredCode,
+                url: "https://google.com",
+                expiresAt: DateTime.Parse("January 1, 2000")
+            );
+
+
+            var response = await _client.GetAsync(
+                ApiRoutes.ShortUrlByCode(expiredCode)
+             );
+
+            Assert.Equal(HttpStatusCode.Gone, response.StatusCode);
+        }
+    }
+}
